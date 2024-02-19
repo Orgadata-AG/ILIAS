753ce4e3b5c343a063909ec0351500902b71669f
Moritz Bunkus <m.bunkus@linet.de>
2024-02-19 11:33:20 +0100

    OpenIdConnect: logout: transfer redirect URL via 'state'

--- logout.php
+++ logout.php
@@ -10,7 +10,23 @@
 * @package ilias-core
 */
 
+/**
+ * @see https://www.php.net/manual/en/function.str-starts-with
+ */
+if (!function_exists('str_starts_with')) {
+    function str_starts_with(string $haystack, string $needle): bool
+    {
+        return strlen($needle) === 0 || strpos($haystack, $needle) === 0;
+    }
+}
+
+if(!empty($_GET['state']) && str_starts_with($_GET['state'], 'https://' . $_SERVER['SERVER_NAME'] . '/')) {
+   header('Location: ' . $_GET['state']);
+   exit;
+}
+
 require_once("Services/Init/classes/class.ilInitialisation.php");
+
 ilInitialisation::initILIAS();
 
 $ilCtrl->setCmd('doLogout');
